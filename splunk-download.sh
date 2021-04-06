b502804202eab51af72ca23d221b6bb08b957cd3
securitytime <72920563+securitytime@users.noreply.github.com>
2021-04-06 15:08:26 +0200

    Create splunk-download.sh

---
+++ splunk-download.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Depending on the user input, this script will download Splunk Enterprise or the Universal Forwarder from splunk.com.
+# Furthermore, the user can choose to download the installer either in the RPM, TGZ or MSI format.
+# The download will be placed inside the current directory.
+
+usage(){
+   echo "Usage: $0 [full|uf] [rpm|tgz|deb|msi]"
+   echo "Example: $0 full rpm"
+   exit 1
+}
+
+if [ "$1" = "full" ]; then
+   url="https://www.splunk.com/en_us/download/splunk-enterprise.html"
+elif [ "$1" = "uf" ]; then
+   url="https://www.splunk.com/en_us/download/universal-forwarder.html"
+else
+   usage
+fi
+
+if [ "$2" = "rpm" ]; then
+   link_tail="linux-2\.6-x86_64\.rpm"
+elif [ "$2" = "tgz" ]; then
+   link_tail="Linux-x86_64\.tgz"
+elif [ "$2" = "deb" ]; then
+   link_tail="linux-2\.6-amd64\.deb"
+elif [ "$2" = "msi" ]; then
+   link_tail="x64-release\.msi"
+else
+   usage
+fi
+
+content=`curl -s --connect-timeout 10 --max-time 10 $url`
+link=`echo $content | egrep -o "data-link=\"https://[^\"]+-${link_tail}\"" | cut -c12- | rev | cut -c2- | rev`
+if [[ "$link" == "https://"* ]]; then
+   echo "Download link: $link"
+   echo ""
+   # If you receive SSL certitifcate errors, then use --no-check-certificate.
+   wget $link
+else
+   echo "Error: Could not get download link from $url. Try again."
+fi
