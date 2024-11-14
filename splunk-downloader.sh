aec758d9e8ea89af64b7c66c56ab35ae8d57892c
securitytime <72920563+securitytime@users.noreply.github.com>
2024-11-14 23:00:56 +0100

    Update splunk-downloader.sh

--- splunk-downloader.sh
+++ splunk-downloader.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# Depending on the user input, this script will download Splunk Enterprise or the Universal Forwarder from splunk.com.
-# Furthermore, the user can choose to download the installer either in the RPM, TGZ, DEB or MSI format.
-# The download will be placed inside the current directory.
-
 # For troubleshooting, use these commands:
 # curl "https://www.splunk.com/en_us/download/splunk-enterprise.html" | grep "data-link"
 # curl "https://www.splunk.com/en_us/download/universal-forwarder.html" | grep "data-link"
 
 usage(){
-   echo "Usage: $0 [full|uf] [rpm|tgz|deb|msi]"
+   echo "Usage: $0 [full|uf] [deb|rpm|tgz|msi]"
    echo "Example: $0 full rpm"
    exit 1
 }
@@ -22,12 +18,12 @@ else
    usage
 fi
 
-if [ "$2" = "rpm" ]; then
+if [ "$2" = "deb" ]; then
+   link_tail="-linux-2\.6-amd64\.deb"
+elif [ "$2" = "rpm" ]; then
    link_tail="\.x86_64\.rpm"
 elif [ "$2" = "tgz" ]; then
    link_tail="-Linux-x86_64\.tgz"
-elif [ "$2" = "deb" ]; then
-   link_tail="-linux-2\.6-amd64\.deb"
 elif [ "$2" = "msi" ]; then
    link_tail="-x64-release\.msi"
 else
