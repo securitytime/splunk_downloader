096e8b709e3a538b20a7a05a5bf94b8868a7e3d4
securitytime <72920563+securitytime@users.noreply.github.com>
2024-12-18 18:24:12 +0100

    Update splunk-downloader.sh

--- splunk-downloader.sh
+++ splunk-downloader.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# For troubleshooting, use these commands:
-# curl "https://www.splunk.com/en_us/download/splunk-enterprise.html" | grep "data-link"
-# curl "https://www.splunk.com/en_us/download/universal-forwarder.html" | grep "data-link"
-
 usage(){
-   echo "Usage: $0 [full|uf] [deb|rpm|tgz|msi]"
-   echo "Example: $0 full rpm"
-   exit 1
+    echo "Usage: $0 [full|uf]"
+    echo "Example: $0 full"
+    exit 1
 }
 
 if [ "$1" = "full" ]; then
@@ -18,25 +14,4 @@ else
    usage
 fi
 
-if [ "$2" = "deb" ]; then
-   link_tail="-linux-2\.6-amd64\.deb"
-elif [ "$2" = "rpm" ]; then
-   link_tail="\.x86_64\.rpm"
-elif [ "$2" = "tgz" ]; then
-   link_tail="-Linux-x86_64\.tgz"
-elif [ "$2" = "msi" ]; then
-   link_tail="-x64-release\.msi"
-else
-   usage
-fi
-
-content=`curl -s --connect-timeout 10 --max-time 10 $url`
-link=`echo $content | grep -E -o "data-link=\"https://[^\"]+${link_tail}\"" | cut -c12- | rev | cut -c2- | rev`
-if [[ "$link" == "https://"* ]]; then
-   echo "Download link: $link"
-   echo ""
-   # If you receive SSL certitifcate errors, then use --no-check-certificate.
-   wget $link
-else
-   echo "Error: Could not get download link from $url. Try again."
-fi
+curl -s --connect-timeout 10 --max-time 10 $url | grep -E -o "data-link=\"https://[^\"]+\"" | cut -c12- | rev | cut -c2- | rev
